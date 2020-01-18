222236ea535b47133e76c9a123ff10420485861c
Yongjun <24871665+Yongjun042@users.noreply.github.com>
2020-01-18 19:40:30 +0900

    new version

--- src/pages/inedx.js
+++
@@ -1,8 +0,0 @@
-import Board from './Board.js';
-import FindAlgo from './FindAlgo.js';
-import Login from './Login.js';
-import NavBar from '../components/NavBar.js/index.js';
-
-export {
-    Board, FindAlgo,Login,NavBar
-};
